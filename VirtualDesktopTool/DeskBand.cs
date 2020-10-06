ab9159c212ae9314482399c32bb656d36038cf27
Aaron Opell <aaron.opell@live.com>
2020-10-06 00:57:20 -0700

    Added deskband files

---
+++ VirtualDesktopTool/DeskBand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using CSDeskBand;
+using CSDeskBand.ContextMenu;
+
+namespace VirtualDesktopTool
+{
+    [ComVisible(true)]
+    [Guid("17F380B7-BE3B-4900-B02F-8137ABACC6F0")]
+    [CSDeskBandRegistration(Name = "Virtual Desktop Tool")]
+    public class DeskBand : CSDeskBandWpf
+    {
+        protected override UIElement UIElement => new DeskBandControl();
+
+        public DeskBand()
+        {
+
+        }
+    }
+}
