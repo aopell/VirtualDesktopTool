6ca9cb904ef0099220dc7c2c863bb8d99bb347c9
Aaron Opell <aaron.opell@live.com>
2020-10-08 11:22:48 -0700

    Icon changing

--- VirtualDesktopTool2/MainWindow.xaml.cs
+++ VirtualDesktopTool2/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -15,7 +16,6 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using WindowsDesktop;
 using Microsoft.Win32;
-using Ookii.Dialogs.Wpf;
 
 namespace VirtualDesktopTool2
 {
@@ -39,19 +39,12 @@ namespace VirtualDesktopTool2
             VirtualDesktop.Created += VirtualDesktop_Created;
             VirtualDesktop.Destroyed += VirtualDesktop_Destroyed;
             VirtualDesktop.CurrentChanged += VirtualDesktop_CurrentChanged;
+            VirtualDesktop.Renamed += VirtualDesktop_Renamed;
 
             UpdateToolbar();
-
-            Tools.CreateShortcut("Test",
-                                 "D:\\aaron\\Documents\\GitHub\\VirtualDesktopTool\\VirtualDesktopTool2\\bin\\Release\\netcoreapp3.1\\Virtual Desktops",
-                                 "notepad.exe",
-                                 "",
-                                 Tools.WindowStyle.Normal,
-                                 "Test thing",
-                                 @"D:\aaron\Documents\GitHub\VirtualDesktopTool\VirtualDesktopTool2\bin\Release\netcoreapp3.1\Icons\desktop.ico"
-            );
         }
 
+        private void VirtualDesktop_Renamed(object sender, VirtualDesktopRenamedEventArgs e) => UpdateToolbar();
         private void VirtualDesktop_CurrentChanged(object sender, VirtualDesktopChangedEventArgs e) => UpdateToolbar();
         private void VirtualDesktop_Destroyed(object sender, VirtualDesktopDestroyEventArgs e) => UpdateToolbar();
         private void VirtualDesktop_Created(object sender, VirtualDesktop e) => UpdateToolbar();
@@ -63,26 +56,46 @@ namespace VirtualDesktopTool2
 
             foreach (int n in nums)
             {
-                Tools.CreateShortcut(n.ToString(),
+                Tools.CreateShortcut($"{n} - {desktops[n - 1].Name}",
                                      Path.GetFullPath(Tools.ToolbarDirectory),
                                      "dotnet.exe",
                                      $"\"{Assembly.GetExecutingAssembly().Location}\" {n}",
                                      Tools.WindowStyle.Minimized,
-                                     $"Desktop {n}",
+                                     string.IsNullOrEmpty(desktops[n - 1].Name) ? $"Desktop {n}" : desktops[n - 1].Name,
                                      Path.GetFullPath($"Icons\\{(n <= 10 ? n.ToString() : "desktop")}.ico")
                 );
             }
 
-            var keepFiles = nums.Select(n => $"{n}.lnk").ToList();
+            int current = Array.IndexOf(desktops, VirtualDesktop.Current) + 1;
+            Tools.CreateShortcut($"{current} - {VirtualDesktop.Current.Name}",
+                                 Path.GetFullPath(Tools.ToolbarDirectory),
+                                 "dotnet.exe",
+                                 $"\"{Assembly.GetExecutingAssembly().Location}\" {current}",
+                                 Tools.WindowStyle.Minimized,
+                                 string.IsNullOrEmpty(desktops[current - 1].Name) ? $"Desktop {current}" : desktops[current - 1].Name,
+                                 Path.GetFullPath($"Icons\\{(current <= 10 ? current.ToString() : "desktop")}-selected.ico")
+            );
+
+            bool deleted = false;
+            var keepFiles = nums.Select(n => $"{n} - {desktops[n - 1].Name}.lnk").ToList();
             foreach (string file in Directory.GetFiles(Tools.ToolbarDirectory).Where(f => !keepFiles.Contains(Path.GetFileName(f))))
             {
+                deleted = true;
                 File.Delete(file);
+            }
+
+            if (deleted)
+            {
+                // Hacky solution for getting the taskbar toolbar to update when the shortcut files are deleted
+                var tempPath = Path.Combine(Tools.ToolbarDirectory, ".vdesktop-temp");
+                File.Create(tempPath).Close();
+                Task.Delay(100).ContinueWith(_ => File.Delete(tempPath));
             }
         }
 
         private void SelectToolbarFolder(object sender, RoutedEventArgs e)
         {
-
+            Process.Start("explorer.exe", Path.GetFullPath(Tools.ToolbarDirectory));
         }
     }
 }
